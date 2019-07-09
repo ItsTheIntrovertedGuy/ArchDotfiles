3e0d8eb427ff5659c3a14967a7fa058c329d6e5b
Felix ItsTheIntrovertedGuy <max0018@t-online.de>
2019-07-09 18:25:06 +0200

    Readded redshift, got rid of 4coder export as I prefer nvim atm

--- .config/sway/start.sh
+++ .config/sway/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Software
-#redshift &
+redshift &
 onedrive -m --syncdir /home/felix/OneDrive &
 mpd &
 
 # Add 4coder path
-export PATH="${PATH}:/home/felix/4coder_default/"
+#export PATH="${PATH}:/home/felix/4coder_default/"
 
 sway
